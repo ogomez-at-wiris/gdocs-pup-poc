633138e5e6b372d10f5b7e2f381a98126bc26709
ogomez-at-wiris <oriol.gomez@wiris.com>
2022-08-29 08:57:07 +0200

    feat:  Add screenshot generated from canvas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Puppeteer = require("puppeteer");
+const util = require('util');
 const env = require('dotenv').config();
 
 const main = async () => {
@@ -18,7 +19,30 @@ const main = async () => {
     await page.keyboard.press('Enter')
     await page.waitForNavigation();
     await page.goto('https://docs.google.com/document/d/1-DXdfm6zTGDWz9Ygw_nYDvHHqnJm8UPudPZNo6C3rSs/edit')
-    console.log('ok!');
+    await page.click('#docs-extensions-menu')
+    const betaElement = await page.waitForXPath('//*[@id="MrVDjJHNf0_EgQS43GItmKTKs1943j4Ss"]/div')
+    await betaElement.click();
+    const element = await page.waitForXPath('//div[contains(., "Open MathType")]/*')
+    await element.click();
+    const frame = await page.waitForSelector('body > div.script-application-sidebar > div.script-application-sidebar-content > iframe');
+    console.log('got frame');
+    const frameContent = await frame.contentFrame();
+    console.log('content');
+    const sandbox = await frameContent.waitForSelector('#sandboxFrame')
+    console.log('got sandbox');
+    const sandboxContent = await sandbox.contentFrame();
+    console.log('got sandbox content');
+    await page.waitForTimeout(10000)
+    // let bodyHTML = await sandboxContent.content();
+    const userFrame = await sandboxContent.waitForSelector('iframe')
+    console.log('got user frame');
+    const userContent = await userFrame.contentFrame()
+    console.log('content');
+    await userContent.waitForSelector('#app');
+    console.log('app found!');
+    const canvas = await page.waitForSelector('canvas');
+    await canvas.screenshot({ path: 'example.png' });
+    console.log('screenshot made')
 }
 
 main();
