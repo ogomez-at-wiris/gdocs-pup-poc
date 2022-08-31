ea3ba552722d3b4e5267d69c68750dbeb643458a
ogomez-at-wiris <oriol.gomez@wiris.com>
2022-08-31 14:07:27 +0200

    feat: Create google docs example

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const main = async () => {
     await page.waitForNavigation();
     await page.goto('https://docs.google.com/document/d/1-DXdfm6zTGDWz9Ygw_nYDvHHqnJm8UPudPZNo6C3rSs/edit')
     await page.click('#docs-extensions-menu')
-    const betaElement = await page.waitForXPath('//*[@id="MrVDjJHNf0_EgQS43GItmKTKs1943j4Ss"]/div')
+    const betaElement = await page.waitForXPath('//*[@id="M7QBcEIXAo8qRwgHek537QAI9Xc9qMK8g"]/div')
     await betaElement.click();
     const element = await page.waitForXPath('//div[contains(., "Open MathType")]/*')
     await element.click();
@@ -38,11 +38,49 @@ const main = async () => {
     console.log('got user frame');
     const userContent = await userFrame.contentFrame()
     console.log('content');
-    await userContent.waitForSelector('#app');
+    let app = await userContent.waitForSelector('#app');
     console.log('app found!');
-    const canvas = await page.waitForSelector('canvas');
-    await canvas.screenshot({ path: 'example.png' });
+    let canvas = await page.waitForSelector('canvas');
+    await canvas.screenshot({
+        path: 'example.png'
+    });
     console.log('screenshot made')
+
+    const openMathtypeButton = await app.waitForXPath('//button[contains(., "Open MathType")]/*')
+    await openMathtypeButton.click();
+
+    console.log('getting frame modal dialog app');
+    await page.waitForTimeout(18000);
+    const modalDialogFrame = await page.waitForXPath('//div[@class="modal-dialog-content script-app-contents"]/iframe')
+    console.log('got iframe');
+
+    const modalDialogFrameContent = await modalDialogFrame.contentFrame();
+    console.log('got content', util.inspect(await modalDialogFrameContent.content()));
+    const modalSandbox = await modalDialogFrameContent.waitForSelector('#sandboxFrame')
+    console.log('got sandbox');
+    const modalSandboxContent = await modalSandbox.contentFrame();
+    console.log('got sandbox content');
+    // let bodyHTML = await sandboxContent.content();
+    const modalUserFrame = await modalSandboxContent.waitForSelector('iframe')
+    console.log('got user frame');
+    const modalUserContent = await modalUserFrame.contentFrame()
+    console.log('content');
+
+    app = await modalUserContent.waitForSelector('#app');
+    console.log('got editor app');
+    const input = await app.waitForSelector('#editorContainer');
+    console.log('got input');
+    await input.type('123');
+    console.log('typed');
+    await app.click('#insert-button')
+    console.log('clicked insert');
+    await page.waitForTimeout(5000);
+    canvas = await page.waitForSelector('canvas');
+    await canvas.screenshot({
+        path: 'example_equation.png'
+    });
+    console.log('screenshot made')
+
 }
 
 main();
